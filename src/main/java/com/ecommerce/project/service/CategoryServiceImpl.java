a6faa1124434f0c9fdc5992f90a009c71c28ace5
gauravstr07 <gauravstr05@gmail.com>
2025-08-24 16:44:14 +0530

    Implemented Sorting

--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import com.ecommerce.project.exceptions.APIException;
 import com.ecommerce.project.exceptions.ResouceNotFoundException;
@@ -26,9 +27,11 @@ public class CategoryServiceImpl implements CategoryService {
 	private ModelMapper modelMapper;
 
 	@Override
-	public CategoryResponse getAllCategories(Integer pageNumber, Integer pageSize) {
+	public CategoryResponse getAllCategories(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder) {
 
-		Pageable pageable = PageRequest.of(pageNumber, pageSize);
+		Sort sortByAndOrder = sortOrder.equals("asc") ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
+
+		Pageable pageable = PageRequest.of(pageNumber, pageSize, sortByAndOrder);
 
 		Page<Category> categoryPage = categoryRepository.findAll(pageable);
 
